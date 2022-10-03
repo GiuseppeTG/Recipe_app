7c48ac7dd3e927e917464c37deb42d2c01e6c85c
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-03 16:14:47 -0500

    Generate food controller migration views model

--- db/migrate/20221003191810_create_recipes.rb
+++ db/migrate/20221003191810_create_recipes.rb
@@ -1,13 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
- def change
-  create_table :recipes do |t|
-   t.string :name, null: false
-   t.integer :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-   t.integer :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-   t.string :description, null: false
-   t.boolean :public, null: false, default: true
-   t.references :user
-   t.timestamps
+  def change
+    create_table :recipes do |t|
+      t.string :name, null: false
+      t.integer :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+      t.integer :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+      t.string :description, null: false
+      t.boolean :public, null: false, default: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
   end
- end
 end
