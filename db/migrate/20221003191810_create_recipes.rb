da11c66de75283ab67706ad3532629cd7e33bad8
Andres Garzon <andgarzonmal@unal.edu.co>
2022-10-03 14:42:41 -0500

    Update 20221003191810_create_recipes.rb

--- db/migrate/20221003191810_create_recipes.rb
+++ db/migrate/20221003191810_create_recipes.rb
@@ -1,13 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :recipes do |t|
-      t.string :name
-      t.integer :preparation_time
-      t.integer :cooking_time
-      t.string :description
-      t.boolean :public
-      t.references :user
-      t.timestamps
-    end
+ def change
+  create_table :recipes do |t|
+   t.string :name, null: false
+   t.integer :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+   t.integer :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+   t.string :description, null: false
+   t.boolean :public, null: false, default: true
+   t.references :user
+   t.timestamps
   end
+ end
 end
