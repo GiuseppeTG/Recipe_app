bb05a36e5e941dd807ef43386a7bcfdc85c5218d
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-04 12:14:42 -0500

    Create method add recipe food

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
   resources :foods
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end
   devise_for :users
   root 'users#index'
   resources :users
-  resources :recipes
+
   resources :public_recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
