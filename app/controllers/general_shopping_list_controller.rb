2e727fcd37bcbdbf5d974c18d8dbfcc58e869d53
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-05 08:42:49 -0500

    navbar

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,4 +1,6 @@
 class GeneralShoppingListController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @foods = Food.where(user: current_user)
     @recipe_foods = RecipeFood.where(recipe: Recipe.where(user: current_user))
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
   before_action :set_recipe, only: %i[show edit update destroy]
 
   # GET /recipes or /recipes.json
